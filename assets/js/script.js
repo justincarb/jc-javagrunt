3bf10984f196026ec9522ae2d62df7f961a0410f
Amrit Singh <direcode@gmail.com>
2015-05-27 15:22:05 -0500

    Message is javascritp

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-function clickme(){
-    var inputDisplay = document.getElementById('input-display');
-    var inputBox = document.getElementById('input-box');
-    inputBox.innerHTML += '<li>' +
+function clickme() {
+  var inputDisplay = document.getElementById('input-display');
+  var inputBox = document.getElementById('input-box');
+  inputBox.innerHTML += '<li id="mail-li">' +
     inputDisplay.value +
     '<a href="#" id ="close-btn" class="closebtn" onclick = "clickClose()">X</a>' +
     '</li></br>';
-  }
+}
 
-function clearInput(){
+function clearInput() {
   var inputBox = document.getElementById('input-box');
   inputBox.innerHTML = '';
 }
 
-function  clickClose(){
+function clickClose() {
   var closeBtn = document.getElementById('close-btn');
-  closeBtn.onclick = function(event){
-    event.parentNode.remove();
+  closeBtn.onclick = function() {
+    this.parentNode.getElementById('input-box').remove();
   }
- }
+}
