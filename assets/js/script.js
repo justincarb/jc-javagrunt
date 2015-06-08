aaf01a33f27febd29a30d70788d1cd3050cb5f39
Amrit Singh <direcode@gmail.com>
2015-06-08 17:59:43 -0500

    New javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,26 @@
-var list = ["Amrit", "Ajimal", "Ajit"];
-var listL = list.length;
-var names ="";
-var inputDisplay = document.getElementById('input-display');
 var inputBox = document.getElementById('input-box');
+var inputDisplay = document.getElementById('input-display');
 
-function clickme() {
-        list.push(inputDisplay);
+
+function clickme(){
+    inputBox.innerHTML += "<li>" + inputDisplay.value + '<a href="#" id="closebtn">x</a></li>';
+}
+
+function clearInput(){
+
+    inputBox.innerHTML = " ";
+}
+
+function delLast(){
+    if( inputBox.innerHTML === "") {
+     alert("There is nothing to delete");
+    
+    }else{
+    var lastInputText = inputBox.lastChild;
+    lastInputText.remove();
     }
 
- for(var i = 0; i < listL; i++){
-        names += "<li>" + list[i] + "</li></br>";
-        }
-inputBox.innerHTML = names ;
-
-function clearInput() {
-  var inputBox = document.getElementById('input-box');
-  inputBox.innerHTML = '';
 }
 
-function clickClose() {
-  var closeBtn = document.getElementById('close-btn');
-  closeBtn.onclick = function() {
-    this.parentNode.getElementById('input-box').remove();
-  }
-}
+var closeThis = document.getElementById("closebtn");
+
