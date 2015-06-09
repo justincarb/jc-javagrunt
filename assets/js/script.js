a5c6a22ecdcd19f82c5dab97cf469ed95786a5ed
direcode 2 <direcode@gmail.com>
2015-06-09 13:59:46 -0500

    javascript changed in Library

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,38 @@
 var inputBox = document.getElementById('input-box');
 var inputDisplay = document.getElementById('input-display');
-
-
+var addText = document.getElementById('add-text');
+var clearInput = document.getElementById('clear-input');
+var delLast = document.getElementById('del-last');
+/*
 function clickme(){
     inputBox.innerHTML += "<li>" + inputDisplay.value + '<a href="#" id="closebtn">x</a></li>';
 }
+*/
+addText.onclick = function(){
+  inputBox.innerHTML += '<li>' + inputDisplay.value + '<a href="#" id="closebtn">x</a></li>';
+};
 
-function clearInput(){
 
-    inputBox.innerHTML = " ";
-}
+clearInput.onclick = function(){
+  inputBox.innerHTML = '';
 
-function delLast(){
-    if( inputBox.innerHTML === "") {
-     alert("There is nothing to delete");
-    
+};
+
+  delLast.onclick = function(){
+    if( inputBox.innerHTML === '') {
+     alert('There is nothing to delete');
+
     }else{
     var lastInputText = inputBox.lastChild;
     lastInputText.remove();
     }
 
-}
+};
 
-var closeThis = document.getElementById("closebtn");
+var closeThis = document.getElementById('closebtn');
+body.addEventListener('change', function(e){
+e.onclick = function(){
+    alert('Yahoo');
+  };
 
+});
